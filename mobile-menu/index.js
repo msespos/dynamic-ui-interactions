f016119e074a17eb2f80c32a7f6d8cf5f45597f9
Mike Esposito <mikeesposito6852@gmail.com>
2022-10-19 18:57:44 -0700

    Add black icons and make white icons clickable to black

---
+++ mobile-menu/index.js
@@ -0,0 +1,34 @@
+const eyeWhite = document.getElementById("eye-white");
+const eyeBlack = document.getElementById("eye-black");
+eyeWhite.onclick = () => {
+  eyeWhite.style.display = "none";
+  eyeBlack.style.display = "block";
+}
+
+const turkeyWhite = document.getElementById("turkey-white");
+const turkeyBlack = document.getElementById("turkey-black");
+turkeyWhite.onclick = () => {
+  turkeyWhite.style.display = "none";
+  turkeyBlack.style.display = "block";
+}
+
+const handWhite = document.getElementById("hand-white");
+const handBlack = document.getElementById("hand-black");
+handWhite.onclick = () => {
+  handWhite.style.display = "none";
+  handBlack.style.display = "block";
+}
+
+const phoneWhite = document.getElementById("phone-white");
+const phoneBlack = document.getElementById("phone-black");
+phoneWhite.onclick = () => {
+  phoneWhite.style.display = "none";
+  phoneBlack.style.display = "block";
+}
+
+const triforceWhite = document.getElementById("triforce-white");
+const triforceBlack = document.getElementById("triforce-black");
+triforceWhite.onclick = () => {
+  triforceWhite.style.display = "none";
+  triforceBlack.style.display = "block";
+}
