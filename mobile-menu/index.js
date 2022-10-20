a8712f2a45f197c7c2de8f00a71d773837bd5d82
Mike Esposito <mikeesposito6852@gmail.com>
2022-10-20 07:08:07 -0700

    Implement resetting other icons to white when icon is clicked

--- mobile-menu/index.js
+++ mobile-menu/index.js
@@ -1,6 +1,20 @@
+const clearIcons = () => {
+  document.getElementById("eye-black").style.display = "none";
+  document.getElementById("eye-white").style.display = "block";
+  document.getElementById("turkey-black").style.display = "none";
+  document.getElementById("turkey-white").style.display = "block";
+  document.getElementById("hand-black").style.display = "none";
+  document.getElementById("hand-white").style.display = "block";
+  document.getElementById("phone-black").style.display = "none";
+  document.getElementById("phone-white").style.display = "block";
+  document.getElementById("triforce-black").style.display = "none";
+  document.getElementById("triforce-white").style.display = "block";
+}
+
 const eyeWhite = document.getElementById("eye-white");
 const eyeBlack = document.getElementById("eye-black");
 eyeWhite.onclick = () => {
+  clearIcons();
   eyeWhite.style.display = "none";
   eyeBlack.style.display = "block";
 }
@@ -8,6 +22,7 @@ eyeWhite.onclick = () => {
 const turkeyWhite = document.getElementById("turkey-white");
 const turkeyBlack = document.getElementById("turkey-black");
 turkeyWhite.onclick = () => {
+  clearIcons();
   turkeyWhite.style.display = "none";
   turkeyBlack.style.display = "block";
 }
@@ -15,6 +30,7 @@ turkeyWhite.onclick = () => {
 const handWhite = document.getElementById("hand-white");
 const handBlack = document.getElementById("hand-black");
 handWhite.onclick = () => {
+  clearIcons();
   handWhite.style.display = "none";
   handBlack.style.display = "block";
 }
@@ -22,6 +38,7 @@ handWhite.onclick = () => {
 const phoneWhite = document.getElementById("phone-white");
 const phoneBlack = document.getElementById("phone-black");
 phoneWhite.onclick = () => {
+  clearIcons();
   phoneWhite.style.display = "none";
   phoneBlack.style.display = "block";
 }
@@ -29,6 +46,7 @@ phoneWhite.onclick = () => {
 const triforceWhite = document.getElementById("triforce-white");
 const triforceBlack = document.getElementById("triforce-black");
 triforceWhite.onclick = () => {
+  clearIcons();
   triforceWhite.style.display = "none";
   triforceBlack.style.display = "block";
 }
