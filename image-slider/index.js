a1723db524b23434882319dd652d94e1bc16a3f0
Mike Esposito <mikeesposito6852@gmail.com>
2022-10-23 18:44:30 -0700

    Make arrows functional

--- image-slider/index.js
+++ image-slider/index.js
@@ -1,1 +1,15 @@
-document.getElementById("test").style.color = "red";
+document.getElementById("arrow-left").onclick = () => {
+  let img = document.querySelector(".images");
+  let imgStyleRight = getComputedStyle(img).right;
+  if (parseInt(imgStyleRight) >= -900) {
+    img.style.right = (parseInt(imgStyleRight) - 300) + "px";
+  }
+};
+
+document.getElementById("arrow-right").onclick = () => {
+  let img = document.querySelector(".images");
+  let imgStyleRight = getComputedStyle(img).right;
+  if (parseInt(imgStyleRight) <= -300) {
+    img.style.right = (parseInt(imgStyleRight) + 300) + "px";
+  }
+};
