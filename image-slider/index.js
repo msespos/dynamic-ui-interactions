097d1b2da0bc40e58f17ddc44e7d2e6b29c6ac0c
Mike Esposito <mikeesposito6852@gmail.com>
2022-10-24 18:25:40 -0700

    Refactor code for arrows to be more DRY and add clearCircleBackgrounds and add code for selecting image by clicking a circle

--- image-slider/index.js
+++ image-slider/index.js
@@ -1,15 +1,37 @@
-document.getElementById("arrow-left").onclick = () => {
-  let img = document.querySelector(".images");
-  let imgStyleRight = getComputedStyle(img).right;
-  if (parseInt(imgStyleRight) > -751) {
-    img.style.right = (parseInt(imgStyleRight) - 250) + "px";
-  }
+const arrows = ["arrow-left", "arrow-right"];
+
+arrows.forEach((arrow, index) => {
+  document.getElementById(arrow).onclick = () => {
+    document.querySelector(".images").style.transition = "right 0.75s linear";
+    let img = document.querySelector(".images");
+    let imgStyleRight = getComputedStyle(img).right;
+    if (index === 0) {
+      if (parseInt(imgStyleRight) > -751) {
+        img.style.right = (parseInt(imgStyleRight) - 250) + "px";
+      }
+    } else if (index === 1) {
+      if (parseInt(imgStyleRight) < 0) {
+        img.style.right = (parseInt(imgStyleRight) + 250) + "px";
+      }
+    }
+  };
+});
+
+const clearCircleBackgrounds = () => {
+  document.querySelectorAll(".circle").forEach((circle) => {
+    circle.style.backgroundColor = "lightgray";
+  });
 };
 
-document.getElementById("arrow-right").onclick = () => {
-  let img = document.querySelector(".images");
-  let imgStyleRight = getComputedStyle(img).right;
-  if (parseInt(imgStyleRight) < 0) {
-    img.style.right = (parseInt(imgStyleRight) + 250) + "px";
-  }
-};
+const circles = ["first-circle", "second-circle", "third-circle",
+                 "fourth-circle", "fifth-circle"];
+const startingPoints = ["-1000px", "-750px", "-500px", "-250px", "0px"];
+
+circles.forEach((circle, index) => {
+  document.getElementById(circle).onclick = () => {
+    document.querySelector(".images").style.transition = "none";
+    document.querySelector(".images").style.right = startingPoints[index];
+    clearCircleBackgrounds();
+    document.getElementById(circle).style.backgroundColor = "gray";
+  };
+});
